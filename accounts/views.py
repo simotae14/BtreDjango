dea6913f7cb2f998fbf6f473920e4b5e00ceb4c7
Simone Taeggi <startae14@gmail.com>
2019-03-06 00:11:55 +0100

    Add login logic and messages

--- accounts/views.py
+++ accounts/views.py
@@ -41,7 +41,16 @@ def register(request):
 def login(request):
     if request.method == 'POST':
         # Login user
-        return
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid credentials')
+            return redirect('login')
     else:
         return render(request, 'accounts/login.html')
 
